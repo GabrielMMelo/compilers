884c05f1e919e3b52d795d40143aeb3c1f12e114
Breno Tiso <brenotiso@hotmail.com>
2019-06-02 20:34:09 -0300

    Mudando a abordagem em vários métodos

--- syntactic/analyzer.py
+++ syntactic/analyzer.py
@@ -16,8 +16,8 @@ class AnalisadorSintatico:
         if self.actual == len(self.tokens):
             self.success = True
 
-    def move_token_backward(self):
-        self.actual -= 1
+    def move_token_backward(self, number=1):
+        self.actual -= number
 
     def match(self, token):
         try:
@@ -32,33 +32,46 @@ class AnalisadorSintatico:
         # throw a error here!
 
     def analisar(self):
-        self.programa()
+        self.success = self.programa()
         if self.success:
             print("success")
+        else:
+            print("error")
+            print("token atual: ", self.actual)
 
     def programa(self):
-        self.declaracaoLista()
+        return self.declaracaoLista()
 
     def declaracaoLista(self):
-        self.declaracao()
-        while self.declaracao():
-            pass
-        return True
+        if self.declaracao():
+            while self.declaracao():
+                pass
+            return (self.actual == len(self.tokens))  # Verifica se leu todos os tokens
+        return False
 
     def declaracao(self):
+        # É necessário guardar a possição atual para caso ocorra erro
+        # poder voltar os tokens que foram lidos.
+        actual_index = self.actual
         if self.var_declaracao():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
+
         if self.fun_declaracao():
             return True
 
         return False
 
     def var_declaracao(self):
+        actual_index = self.actual
         if self.tipo_especificador():
             if self.ident():
                 if self.match(';'):
                     return True
 
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.tipo_especificador():
             if self.ident():
                 if self.match('['):
@@ -68,6 +81,8 @@ class AnalisadorSintatico:
                                 pass
                             if self.match(';'):
                                 return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def tipo_especificador(self):
@@ -80,21 +95,29 @@ class AnalisadorSintatico:
         #    return True
         if self.match('VOID'):
             return True
+        actual_index = self.actual
         if self.match('STRUCT'):
             if self.ident():
                 if self.match('{'):
                     if self.atributos_declaracao():
                         if self.match('}'):
                             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def atributos_declaracao(self):
-        self.var_declaracao()
-        while self.var_declaracao():
-            pass
-        return True
+        actual_index = self.actual
+        if self.var_declaracao():
+            while self.var_declaracao():
+                pass
+            return True
+
+        self.move_token_backward(self.actual - actual_index)
+        return False
 
     def fun_declaracao(self):
+        actual_index = self.actual
         if self.tipo_especificador():
             if self.ident():
                 if self.match('('):
@@ -102,79 +125,132 @@ class AnalisadorSintatico:
                         if self.match(')'):
                             if self.composto_decl():
                                 return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def params(self):
+        actual_index = self.actual
         if self.param_lista():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.match('VOID'):
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def param_lista(self):
+        actual_index = self.actual
         if self.param():
-            while self.match(',') and self.param():
-                pass
+            notEnd = True
+            while notEnd:
+                actual_index = self.actual
+                notEnd = self.match(',')
+                if notEnd:
+                    if not self.param():
+                        self.move_token_backward(self.actual - actual_index)
+                        notEnd = False
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     # alterei a ordem das derivações da gramatica p/ rodar
     def param(self):
+        actual_index = self.actual
         if self.tipo_especificador():
             if self.ident():
                 if self.match('['):
                     if self.match(']'):
                         return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.tipo_especificador():
             if self.ident():
                 return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def composto_decl(self):
+        actual_index = self.actual
         if self.match('{'):
             if self.local_declaracoes():
                 if self.comando_lista():
                     if self.match('}'):
                         return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def local_declaracoes(self):
-        if self.match('{'):
-            if self.var_declaracao():
-                if self.match('}'):
-                    return True
-        return False
+        notEnd = True
+        while notEnd:
+            actual_index = self.actual
+            if not self.var_declaracao():
+                self.move_token_backward(self.actual - actual_index)
+                notEnd = False
+
+        return True
 
     def comando_lista(self):
-        if self.match('{'):
-            if self.comando():
-                if self.match('}'):
-                    return True
-        return False
+        notEnd = True
+        while notEnd:
+            actual_index = self.actual
+            if not self.comando():
+                self.move_token_backward(self.actual - actual_index)
+                notEnd = False
+
+        return True
 
     def comando(self):
+        actual_index = self.actual
         if self.expressao_decl():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.composto_decl():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.selecao_decl():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.iteracao_decl():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.retorno_decl():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def expressao_decl(self):
+        actual_index = self.actual
         if self.expressao():
             if self.match(';'):
                 return True
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.match(';'):
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     # alterei a ordem das derivações da gramatica p/ rodar
     def selecao_decl(self):
+        actual_index = self.actual
         if self.match('IF'):
             if self.match('('):
                 if self.expressao():
@@ -183,44 +259,65 @@ class AnalisadorSintatico:
                             if self.match('ELSE'):
                                 if self.comando():
                                     return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.match('IF'):
             if self.match('('):
                 if self.expressao():
                     if self.match(')'):
                         if self.comando():
                             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def iteracao_decl(self):
+        actual_index = self.actual
         if self.match('WHILE'):
             if self.match('('):
                 if self.expressao():
                     if self.match(')'):
                         if self.comando():
                             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def retorno_decl(self):
+        actual_index = self.actual
         if self.match('RETURN'):
             if self.match(';'):
                 return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.match('RETURN'):
             if self.expressao():
                 if self.match(';'):
                     return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def expressao(self):
+        actual_index = self.actual
         if self.var():
             if self.match('='):
                 if self.expressao():
                     return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.expressao_simples():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     # alterei a ordem das derivações da gramatica p/ rodar
     def var(self):
+        actual_index = self.actual
         if self.ident():
             if self.match('['):
                 if self.expressao():
@@ -228,17 +325,28 @@ class AnalisadorSintatico:
                         while self.match('[') and self.expressao() and self.match(']'):
                             pass
                         return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.ident():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def expressao_simples(self):
+        actual_index = self.actual
         if self.expressao_soma():
             if self.relacional():
                 if self.expressao_soma():
                     return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.expressao_soma():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def relacional(self):
@@ -257,10 +365,19 @@ class AnalisadorSintatico:
         return False
 
     def expressao_soma(self):
+        actual_index = self.actual
         if self.termo():
-            while self.soma() and self.termo():
-                pass
+            notEnd = True
+            while notEnd:
+                notEnd = self.soma()
+                if notEnd:
+                    actual_index = self.actual
+                    if not self.termo():
+                        self.move_token_backward(self.actual - actual_index)
+                        notEnd = False
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def soma(self):
@@ -271,10 +388,19 @@ class AnalisadorSintatico:
         return False
 
     def termo(self):
+        actual_index = self.actual
         if self.fator():
-            while self.mult() and self.fator():
-                pass
+            notEnd = True
+            while notEnd:
+                notEnd = self.mult()
+                if notEnd:
+                    actual_index = self.actual
+                    if not self.fator():
+                        self.move_token_backward(self.actual - actual_index)
+                        notEnd = False
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def mult(self):
@@ -285,26 +411,44 @@ class AnalisadorSintatico:
         return False
 
     def fator(self):
+        actual_index = self.actual
         if self.match('('):
             if self.expressao():
                 if self.match(')'):
                     return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.var():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.ativacao():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.num():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
+        actual_index = self.actual
         if self.num_int():
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def ativacao(self):
+        actual_index = self.actual
         if self.ident():
             if self.match('('):
                 if self.args():
                     if self.match(')'):
                         return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     # derivação opcional apenas
@@ -313,44 +457,39 @@ class AnalisadorSintatico:
         return True
 
     def arg_lista(self):
+        actual_index = self.actual
         if self.expressao():
-            while self.match(',') and self.expressao():
-                pass
+            notEnd = True
+            while notEnd:
+                notEnd = self.match(',')
+                if notEnd:
+                    actual_index = self.actual
+                    if not self.expressao():
+                        self.move_token_backward(self.actual - actual_index)
+                        notEnd = False
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def num(self):
+        actual_index = self.actual
         self.soma()  # é opcional
-        if self.digito():
-            while self.digito():
-                pass
-            self.move_token_backward()
+        if self.num_int():
             if self.match('.'):
-                if self.digito():
-                    while self.digito():
-                        pass
-                    self.move_token_backward()
-                else:
+                if not self.num_int():
                     return False
             if self.match('E'):
                 self.soma()  # é opcional
-                if self.digito():
-                    while self.digito():
-                        pass
-                    self.move_token_backward()
-                else:
+                if not self.num_int():
                     return False
             return True
+
+        self.move_token_backward(self.actual - actual_index)
         return False
 
     def num_int(self):
-        if self.digito():
-            while self.digito():
-                pass
-            return True
-        return False
-
-    def digito(self):
+        # É possível fazer o match aqui já que fornecemos o token INT
         return self.match('INT')
 
     def ident(self):
